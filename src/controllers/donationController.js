986f0bb4222051fc3e6da66b6d45c2edb734599f
ShivanshGera <shivanshgera15@gmail.com>
2026-06-03 15:14:55 +0530

    add donation management routes

---
+++ src/controllers/donationController.js
@@ -0,0 +1,220 @@
+const Donation = require("../models/Donation");
+
+exports.createDonation = async (req,res) =>{
+    try{
+
+        const {foodName,quantity,pickupBy,location} = req.body;
+
+        if(!foodName || !quantity || !pickupBy || !location){
+            return res.status(400).json({
+                success:false,
+                message:"all fields are required",
+            });
+        }
+
+        if(new Date(pickupBy) <= new Date()){
+            return res.status(400).json({
+                success:false,
+                message:"Pick up date must be in the future",
+            });
+        }
+        console.log("REQ USER- ",req.user)
+
+        const donation = await Donation.create({
+            foodName,
+            quantity,
+            pickupBy,
+            location,
+            postedBy: req.user._id
+        })
+
+        return res.status(201).json({
+            success:true,
+            message:"Donation Created successfully",
+            donation,
+        });
+
+    }catch(error){
+        console.error("create donation error- ",error);
+        return res.status(500).json({
+            success:false,
+            message:"Failed to Create Donation",
+            error:error.message,
+        });
+    }
+}
+
+exports.getAvailableDonations = async (req,res) =>{
+    try{
+
+        const page = parseInt(req.query.page) || 1;
+        const limit  = parseInt(req.query.limit) || 10;
+
+        const donations = await Donation.find({
+            status:"available",
+            pickupBy:{$gt : new Date()},
+        })
+        .populate("postedBy" , "name email")
+        .skip((page-1) * limit)
+        .limit(limit)
+        .sort({createdAt :-1});
+
+        return res.status(200).json({
+            success:true,
+            page,
+            limit,
+            donations,
+        });
+
+    }catch(error){
+        console.error("error- ", error)
+        return res.status(500).json({
+           success:false,
+           message:"failed to fetch donations",
+           error:error.message, 
+        });
+    }
+}
+
+
+exports.claimDonations = async (req,res) => {
+    try{
+        const donationId = req.params.id;
+
+        const donation = await Donation.findById(donationId);
+
+        if(!donation){
+            return res.status(404).json({
+                success:false,
+                message:"Donation Not found",
+            });
+        }
+
+        if(donation.postedBy.toString() === req.user._id.toString()){
+            return res.status(400).json({
+                success:false,
+                message:"You Cannot claim your own donation",
+            });
+        }
+
+        if(donation.status !== "available"){
+            return res.status(400).json({
+                success:false,
+                message:"Donation already claimed",
+            });
+        }
+
+        donation.status = "claimed";
+        donation.claimedBy = req.user._id;
+
+        await donation.save();
+
+        return res.status(200).json({
+            success:true,
+            message:"donation claimed successfully",
+            donation,
+        });
+
+    }catch(error){
+        console.log("Error- ",error);
+        return res.status(500).json({
+            success:false,
+            message:"Failed to claim donation",
+            error: error.message,
+
+        });
+
+    }
+};
+
+
+exports.collectDonation = async (req,res) =>{
+    try{
+        const donationId  = req.params.id;
+
+        const donation = await Donation.findById(donationId);
+        if(!donation){
+            return res.status(404).json({
+                success:false,
+                message:"donation not found",
+            });
+        };
+
+        if(donation.postedBy.toString()!== req.user._id.toString()){
+            return res.status(403).json({
+                success:false,
+                message:"Not authorized to collect this donation",
+            });
+        }
+
+        if(donation.status !== "claimed"){
+            return res.status(400).json({
+                success:false,
+                message:"Donation must be claimed first",
+            });
+        }
+
+        donation.status = "collected";
+        await donation.save();
+
+        return res.status(200).json({
+            success:true,
+            message:"Donation collected successfully",
+            donation,
+        });
+        
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"failed to collect donation",
+            error: error.message,
+        });
+    }
+}
+
+
+exports.getMyDonations = async (req,res) =>{
+    try{
+        const donations = await Donation.find({
+            postedBy : req.user._id,
+        })
+        .sort({createdAt:-1});
+
+        return res.status(200).json({
+            success:true,
+            message:"donations fetched",
+            donations,
+        })
+    }catch(error){
+        console.error("error- ",error);
+        return res.status(500).json({
+            success:false,
+            message:"Could not fetch donations",
+            error:error.message,
+        });
+    }
+}
+
+
+exports.getClaimedDonations = async(req,res) =>{
+    try{
+        const donations = await Donation.find({
+            claimedBy:req.user._id,
+        })
+        .populate("postedBy" , "name email")
+        .sort({createdAt:-1});
+
+        return res.status(200).json({
+            success:true,
+            message:"Claimed Donations fetched",
+            donations,
+        })
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:"Failed to see claimed donations",
+            error:error.message,
+        })
+    }
+}
---
+++ src/routes/donationRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {createDonation,getAvailableDonations,claimDonations,collectDonation,getMyDonations, getClaimedDonations} = require("../controllers/donationController");
+const {auth} = require("../middleware/authMiddleware");
+const {isRestaurant} = require("../middleware/roleMiddleware");
+const {isNGO} = require("../middleware/roleMiddleware");
+
+
+
+router.post("/create" , auth, isRestaurant, createDonation );
+router.get("/my-donations" , auth , isRestaurant , getMyDonations);
+router.get("/claimed", auth , isNGO , getClaimedDonations);
+
+
+router.get("/available", auth , isNGO , getAvailableDonations);
+
+
+
+router.post("/:id/claim", auth, isNGO, claimDonations)
+
+
+
+router.patch("/:id/collect", auth ,isRestaurant ,collectDonation);
+
+module.exports = router;
