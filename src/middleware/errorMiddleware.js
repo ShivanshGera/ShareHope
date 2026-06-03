90e126280bdd09ba475c9ab78e827b9b33e2cbd6
ShivanshGera <shivanshgera15@gmail.com>
2026-06-03 15:12:52 +0530

    handle API errors centrally

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,9 @@
+exports.errorHandler = (err, req, res, next) => {
+    console.error(err);
+
+    return res.status(500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+};
+
